8cb87ca1f1a8b4eb3616fb5427a74a8d073614df
Javed Ali <Javedali90045@gmail.com>
2025-11-10 00:19:01 +0530

    used string methods

--- $4_Practice -set.js
+++ $4_Practice -set.js
@@ -3,8 +3,31 @@ console.log(`********** What will the following print in javascript ? console.lo
 console.log("har\"".length);
 
 console.log("Explore the includes, startwith & endwith functions of a string.");
-var name = "Kalyan bail bazar";
-let data = name.includes("bail");
-console.log(data);
-console.log(name.slice(5)); // n bail bazar
-console.log(name.slice(5, 8)); // n b
+
+let text = "JavaScript is fun!";
+console.log(text.includes("Script"));   // true
+console.log(text.includes("script"));   // false (case-sensitive)
+console.log(text.includes("Java", 1));  // false (search starts after index 1)
+
+let word = "Frontend Developer";
+
+console.log(word.startsWith("Front"));    // true
+console.log(word.startsWith("front"));    // false (case-sensitive)
+console.log(word.startsWith("Developer", 9)); // true (starts checking from index 9)
+
+
+console.log("*********** Write a program to convert a given string to lowercase.");
+let nm = "RAJA";
+console.log(nm.toLowerCase());
+
+console.log(`*********** Extract the amount out of this string "please give Rs 1000".`);
+let st = "please give Rs 1000";
+console.log(st.slice(15));
+
+console.log(`*********** Try to change 4th character of a given string were you able to do it ? `);
+let str = "Hello";
+str[3] = "p";    // trying to change the 4th character (index 3)
+console.log(str);
+
+
+
