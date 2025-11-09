ba95f47ff49c18f55671941648d68b515eb32682
Javed Ali <Javedali90045@gmail.com>
2025-11-09 23:59:12 +0530

    added chapter 4

---
+++ $4_Practice -set.js
@@ -0,0 +1,10 @@
+console.log("Practice number 4");
+console.log(`********** What will the following print in javascript ? console.log("har\"".length) **********`);
+console.log("har\"".length);
+
+console.log("Explore the includes, startwith & endwith functions of a string.");
+var name = "Kalyan bail bazar";
+let data = name.includes("bail");
+console.log(data);
+console.log(name.slice(5)); // n bail bazar
+console.log(name.slice(5, 8)); // n b
