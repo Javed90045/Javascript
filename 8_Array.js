bd94b6e372a0a33fc62400f0e2862a9ed0b1350e
Javed Ali <Javedali90045@gmail.com>
2025-11-11 00:16:55 +0530

    learned array methods

--- 8_Array.js
+++ 8_Array.js
@@ -1,1 +1,13 @@
 console.log("Array");
+let num = [1,2,3,4,5,6];
+console.log(num.push(7)); // Adds a new element at the end of the array
+console.log(num);
+console.log(num.length); // length method returns the length of the array
+
+console.log(num.toString()); // convert an array to a string of comma separated values
+
+let n = num.join("-"); // joins all the array elements using a separator
+let nm = num.pop(); // removes last element  from the array and updates the original 
+
+console.log(nm);
+
