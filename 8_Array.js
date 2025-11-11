bd679abb118c409bb5d96bde444ca1ab97d50269
Javed Ali <Javedali90045@gmail.com>
2025-11-12 00:48:32 +0530

    learned some array functions

--- 8_Array.js
+++ 8_Array.js
@@ -1,5 +1,5 @@
 console.log("Array");
-let num = [1,2,3,4,5,6];
+let num = [1,6,5,4,3,2];
 console.log(num.push(7)); // Adds a new element at the end of the array
 console.log(num);
 console.log(num.length); // length method returns the length of the array
@@ -9,5 +9,16 @@ console.log(num.toString()); // convert an array to a string of comma separated 
 let n = num.join("-"); // joins all the array elements using a separator
 let nm = num.pop(); // removes last element  from the array and updates the original 
 
-console.log(nm);
+console.log(num.shift()); //removes first element of array and update the original array 
+console.log(num);
+console.log(num.unshift(1)); // Add element in the begining and returns the new length
+console.log(num);
+delete num[0];  //delete is an oprator that is used to delete element from the array
+console.log(num);
+console.log(num.sort()); // sort() method is used to sort an array alphabetically.
+console.log(num);
+
+num.splice(2,1,10,20); // Splice can be to add new items 
+console.log(num);
+
 
